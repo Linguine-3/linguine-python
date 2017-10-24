67852c43334c8f3263551f403ad5a7e1be28eed5
meyersbs <meyers.bs@gmail.com>
2017-10-24 10:46:59 -0400

    Fixed n-gram visualization showing spaces as tokens.

--- linguine/ops/remove_silent_pauses.py
+++ linguine/ops/remove_silent_pauses.py
@@ -4,13 +4,27 @@ Removes all silent pause tokens, {SL}, from a text.
 Returns the text as a single string separated by spaces.
 """
 
+import re
+
 class RemoveSilence:
     def run(self, data):
         results = []
         for corpus in data:
             split_string = corpus.contents.split(" ")
+            print(split_string)
             temp_corpus = list(filter(("{SL}").__ne__, split_string))
             temp_corpus = list(filter(("{sl}").__ne__, temp_corpus))
-            corpus.contents = " ".join(temp_corpus)
+            temp_corpus = " ".join(temp_corpus)
+            print(temp_corpus)
+            temp_corpus = re.sub(r'\{SL\}\.', '.', temp_corpus)
+            temp_corpus = re.sub(r'\{sl\}\.', '.', temp_corpus)
+            temp_corpus = re.sub(r' +', ' ', temp_corpus)
+            temp_corpus = temp_corpus.split(" ")
+            print(temp_corpus)
+            temp_corpus = list(filter((" ").__ne__, temp_corpus))
+            temp_corpus = list(filter((".").__ne__, temp_corpus))
+            temp_corpus = " ".join(temp_corpus)
+            print(temp_corpus)
+            corpus.contents = temp_corpus
             results.append(corpus)
         return results
