62af4047ca25561094fe7e9cbb37388b23546add
aphedges <aphedges1@gmail.com>
2019-03-01 17:43:32 -0500

    Rewrote some more CoreNLP logic for clarity and efficiency

--- linguine/ops/StanfordCoreNLP.py
+++ linguine/ops/StanfordCoreNLP.py
@@ -14,8 +14,6 @@ class StanfordCoreNLP:
     def __init__(self, analysis_type):
         self.analysis_type = analysis_type
 
-        # print("ANALYSIS: " + str(analysisType))
-
         if StanfordCoreNLP.proc is None:
             StanfordCoreNLP.proc = CoreNLP(configdict={
                 'annotators': 'tokenize, ssplit, pos, lemma, ner, parse, sentiment, dcoref, relation, natlog, openie'},
@@ -23,25 +21,11 @@ class StanfordCoreNLP:
                                            '../../../stanford_corenlp_pywrapper/stanford_corenlp_pywrapper/lib/*')])
 
     def run(self, data):
-        result = self.json_cleanup(data, self.type_to_annotator(self.analysis_type))
+        result = self.json_cleanup(data, self.analysis_type)
         # print(result)
         return result
 
-    def type_to_annotator(self, analysis_type):
-        if analysis_type == 'pos':
-            return ['pos']
-        elif analysis_type == 'ner':
-            return ['ner']
-        elif analysis_type == 'sentiment':
-            return ['parse', 'sentiment']
-        elif analysis_type == 'coref':
-            return ['coref']
-        elif analysis_type == 'relation':
-            return ['parse', 'relation']
-        else:
-            return None
-
-    def json_cleanup(self, data, analysis_types):
+    def json_cleanup(self, data, analysis_type):
         """
         When the JSON segments return from the CoreNLP library, they
         separate the data acquired from each word into their own element.
@@ -58,32 +42,33 @@ class StanfordCoreNLP:
                 words = []
                 for index, token in enumerate(sentence_res["tokens"]):
                     word = {"token": token}
-                    if not any(x in analysis_types for x in ['coref', 'pos', 'relation']):
-                        for atype in analysis_types:
-                            if atype is "sentiment":
-                                word['sentiment'] = sentence_res['sentiments'][index]
-                            elif atype not in ["parse", "relation"]:
-                                word[atype] = sentence_res[atype][index]
+
+                    if analysis_type == "sentiment":
+                        word['sentiment'] = sentence_res['sentiments'][index]
+                    elif analysis_type == 'ner':
+                        word['ner'] = sentence_res['ner'][index]
 
                     words.append(word)
+
                 sentence = {'tokens': words}
-                if "sentiment" in analysis_types:
+
+                if analysis_type == "sentiment":
                     sentence['sentiment'] = sentence_res['sentiment']
                     sentence['sentimentValue'] = sentence_res['sentimentValue']
                     sentence['tree_json'] = json.loads(sentence_res['sentiment_json'])
 
-                if "parse" in analysis_types:
+                if analysis_type in ['sentiment', 'relation']:
                     sentence["parse"] = sentence_res["parse"]
 
-                if "relation" in analysis_types:
+                if analysis_type == "relation":
                     sentence['relations'] = json.loads(sentence_res['relations'])
 
-                if 'pos' in analysis_types:
+                if analysis_type == 'pos':
                     sentence['tree_json'] = json.loads(sentence_res['deps_json'])
 
                 sentences.append(sentence)
 
-        if 'coref' in analysis_types:
+        if analysis_type == 'coref':
             return {'sentences': sentences, "entities": res["entities"]}
         else:
             return {"sentences": sentences}
